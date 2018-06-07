4f71ca0f1a105331a3184aef044d5dc10855352a
by09115 <by09115@outlook.kr>
2018-06-07 19:38:47 +0900

    한 줄만 남기고 다 지웠어요 헤헤

--- Helloworld.c
+++ Helloworld.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
 
 int main() {
-    int WANT_ENTER_DMS, jaehoon;
     printf("Hello, World!\n");
-    printf("Americano is so good!");
-    printf("I'm so sleepy...");
     return 0;
 }
