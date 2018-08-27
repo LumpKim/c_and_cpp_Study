929048683903d4c18ae28ff1d23d79faf2904f9b
by09115 <by09115@outlook.kr>
2018-08-27 16:00:44 +0900

    함수 선언 추가

---
+++ SeongGeun_cpp/cppstudy_0827_sizeofthis.cpp
@@ -0,0 +1,10 @@
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+void main() {
+	cout << "int : " << sizeof(int) << endl;
+	cout << "char : " << sizeof(char) << endl;
+	cout << "short : " << sizeof(short) << endl;
+	cout << "long : " << sizeof(long) << endl;
+}
