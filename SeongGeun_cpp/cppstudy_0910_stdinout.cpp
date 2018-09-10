e59ddfb01863a759c4b8b751b9f6f43f1cf0fb75
by09115 <by09115@outlook.kr>
2018-09-10 13:54:12 +0900

    Today I Learned about cpp with Seonggeun Teacher!

---
+++ SeongGeun_cpp/cppstudy_0910_stdinout.cpp
@@ -0,0 +1,16 @@
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a;
+	cout << "정수 a = ";
+	cin >> a;
+	cout << "정수 a = " << a << "이다" << endl;
+	float b;
+	cout << "실수 b = " << "\n";
+	cin >> b;
+	cout << "실수 b = " << b << "이다" << "\n";
+
+	return 0;
+}
